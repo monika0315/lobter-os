7734d8a3aa25b30fadd3d4b2072b0a90eb4667f1
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 01:50:44 -0300

    reorganise

--- bootstrap.lua
+++ bootstrap.lua
@@ -3,12 +3,13 @@ local branch = "dev"
 local tree_url = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
 local raw_url = "https://raw.githubusercontent.com/" .. repo .. "/refs/heads/" .. branch .. "/"
 
+fs.delete("bootstrap.bak.lua")
+fs.move("bootstrap.lua", "bootstrap.bak.lua")
+
 local tree_res = http.get(tree_url)
 local tree_json = tree_res.readAll()
 tree_res.close()
 
-fs.delete("bootstrap.bak.lua")
-fs.move("bootstrap.lua", "bootstrap.bak.lua")
 local index = textutils.unserializeJSON(tree_json)
 for i, entry in ipairs(index.tree) do
     if entry.type == "blob" then
