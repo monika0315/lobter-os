32d1ca0dbe2b2420bc63b534d1b3d4bd0b345308
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 01:03:38 -0300

    query github json

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,9 @@
-local user = "monika0315"
-local repo = "lobter-os"
+local repo = "monika0315/lobter-os"
 local branch = "dev"
-local file_index = "https://api.github.com/repos/" . user . "/" . repo . "/git/trees/" . branch . "?recursive=1"
+local file_index = "https://api.github.com/repos/" . repo . "/git/trees/" . branch . "?recursive=1"
+
+local h = http.get(url)
+local json = h.readAll()
+h.close()
+
+print(json)
