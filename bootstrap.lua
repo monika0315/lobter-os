8a0f9b1e3e4c7687cb6f38d783663a51aba37489
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 00:55:38 -0300

    initial push

---
+++ bootstrap.lua
@@ -0,0 +1,4 @@
+local user = "monika0315"
+local repo = "lobter-os"
+local branch = "dev"
+local file_index = "https://api.github.com/repos/" . user . "/" . repo . "/git/trees/" . branch . "?recursive=1"
