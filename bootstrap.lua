0aa6ce099395dc470e838d32452f430dd09cf7a5
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 01:08:44 -0300

    fix variable name

--- bootstrap.lua
+++ bootstrap.lua
@@ -2,7 +2,7 @@ local repo = "monika0315/lobter-os"
 local branch = "dev"
 local file_index = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
 
-local h = http.get(url)
+local h = http.get(file_index)
 local json = h.readAll()
 h.close()
 
