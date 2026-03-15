9b30ba11ce4dc6d76b240841583c81d6e47264a2
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 02:31:51 -0300

    add bootstrap config

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,20 +1,27 @@
-local repo = "monika0315/lobter-os"
-local branch = "dev"
-local install_root = "/lobter"
+function read_string(source)
+    local content = source.readAll()
+    source.close()
+    return content
+end
+
+local file = read_string(fs.open("bootstrap.json", "r"))
+local config = textutils.unserialiseJSON(file)
+local repo = config.repo
+local branch = config.branch
+local install_path = config.install_path
+
+fs.makeDir(install_path)
+shell.setDir(install_path)
+for _, file in ipairs(fs.list(install_path)) do
+    fs.delete(fs.combine(install_path, file))
+end
+
 local tree_url = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
 local raw_url = "https://raw.githubusercontent.com/" .. repo .. "/refs/heads/" .. branch .. "/"
 
-fs.makeDir(install_root)
-shell.setDir(install_root)
-for _, file in ipairs(fs.list(install_root)) do
-    fs.delete(fs.combine(install_root, file))
-end
-
-local tree_res = http.get(tree_url)
-local tree_json = tree_res.readAll()
-tree_res.close()
-
+local tree_json = read_string(http.get(tree_url))
 local index = textutils.unserializeJSON(tree_json)
+
 for i, entry in ipairs(index.tree) do
     if entry.type == "blob" then
         local path = entry.path
