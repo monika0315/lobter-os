6a2d7be0696f63c852032fc5707728e16b7c8758
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 02:47:00 -0300

    add config json

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,14 +1,12 @@
-function read_string(source)
-    local content = source.readAll()
-    source.close()
-    return content
-end
-
 local file = read_string(fs.open("/disk/bootstrap.json", "r"))
 local config = textutils.unserialiseJSON(file)
 local repo = config.repo
 local branch = config.branch
 local install_path = config.install_path
+local tree_url = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
+local raw_url = "https://raw.githubusercontent.com/" .. repo .. "/refs/heads/" .. branch .. "/"
+local tree_json = read_string(http.get(tree_url))
+local index = textutils.unserializeJSON(tree_json)
 
 fs.makeDir(install_path)
 shell.setDir(install_path)
@@ -16,16 +14,16 @@ for _, file in ipairs(fs.list(install_path)) do
     fs.delete(fs.combine(install_path, file))
 end
 
-local tree_url = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
-local raw_url = "https://raw.githubusercontent.com/" .. repo .. "/refs/heads/" .. branch .. "/"
-
-local tree_json = read_string(http.get(tree_url))
-local index = textutils.unserializeJSON(tree_json)
-
 for i, entry in ipairs(index.tree) do
     if entry.type == "blob" then
         local path = entry.path
         fs.makeDir(fs.getDir(path))
         shell.run("wget", raw_url .. path, path)
     end
+end
+
+function read_string(source)
+    local content = source.readAll()
+    source.close()
+    return content
 end
