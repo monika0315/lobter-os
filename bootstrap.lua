2e95e89d0b8ecce835d32442ddfa44702408c0b6
Monika Trenholme <monika-trenholme@protonmail.com>
2026-03-15 01:07:46 -0300

    fix string concat

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,6 +1,6 @@
 local repo = "monika0315/lobter-os"
 local branch = "dev"
-local file_index = "https://api.github.com/repos/" . repo . "/git/trees/" . branch . "?recursive=1"
+local file_index = "https://api.github.com/repos/" .. repo .. "/git/trees/" .. branch .. "?recursive=1"
 
 local h = http.get(url)
 local json = h.readAll()
